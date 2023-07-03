fc59936c43625d2ced632acaf2fd8602254086c9
jheymis rodrigues <rodriguesjheymis@gmail.com>
2023-07-03 19:00:03 -0300

    LocalStorage

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
 const tema = localStorage.getItem("tema");
-console.log(tema)
-
+const form = document.getElementById('login_form');
 const mode = document.getElementById('mode_icon');
+
+    
+console.log(tema)
+if(tema === 'dark') {
+  form.classList.add('dark')
+  mode.classList.remove('fa-moon');
+    mode.classList.add('fa-sun');
+}
+  else 
+  if(tema==='light') {
+    form.classList.remove('dark')
+    mode.classList.add('fa-moon');
+  mode.classList.remove('fa-sun');
+  }
+
   mode.addEventListener('click', () => {
-    const form = document.getElementById('login_form');
-    if(tema === 'dark') {
-      form.classList.add('dark')
-    }
-      else{form.classList.remove('dark')}
+    
     if(mode.classList.contains('fa-moon')) {
-    mode.classList.remove('fa-moon');
+   mode.classList.remove('fa-moon');
     mode.classList.add('fa-sun');
 
       form.classList.add('dark');
